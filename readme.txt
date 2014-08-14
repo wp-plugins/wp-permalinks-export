a52cbfb9cdf434cb64fbadaedcea0ecdde120476
TigrouMeow <TigrouMeow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 00:58:54 +0000

    First release.
    git-svn-id: https://plugins.svn.wordpress.org/wp-permalinks-export/trunk@965478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== WP Permalinks Export ===
+Contributors: TigrouMeow
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JAWE2XWH7ZE5U
+Tags: management, admin, export, posts, pages
+Requires at least: 3.9.0
+Tested up to: 3.9.2
+Stable tag: 0.1
+
+Export all permalinks (posts, pages) along with titles.
+
+== Description ==
+
+Languages: English.
+
+== Installation ==
+
+1. Upload `wp-permalinks-export` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. In 'Plugins', click on 'Export Permalinks'.
+
+== Upgrade Notice ==
+
+Replace all the files. Nothing else to do.
+
+== Frequently Asked Questions ==
+
+= Can I contact you? =
+Please contact me through <a href='http://www.meow.fr'>my website</a>. Thanks!
+
+== Screenshots ==
+
+1. Media -> Clean
+
+== Changelog ==
+
+= 0.1 =
+* First release.
---
+++ wp-permalinks-export.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Plugin Name: WP Permalinks Export
+Plugin URI: http://blog.meow.fr/wp-permalinks-export
+Description: Export all permalinks (posts, pages) along with titles.
+Version: 0.1
+Author: Jordy Meow
+Author URI: http://www.meow.fr
+
+Remarks: 
+This plugin was inspired by a StackOverflow thread: 
+http://stackoverflow.com/questions/3464701/export-list-of-pretty-permalinks-and-post-title
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+
+Originally developed for two of my websites: 
+- Totoro Times (http://www.totorotimes.com) 
+- Haikyo (http://www.haikyo.org)
+*/
+
+if ( is_admin() ) {
+
+	class Meow_Permalink_List {
+
+		public function __construct() {
+			add_filter( 'plugin_row_meta', array( $this, 'plugin_meta_links' ), 10, 2 );
+			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		}
+
+		public function plugin_meta_links( $links, $file ) {
+			if ( plugin_basename( __FILE__ ) !== $file )
+				return $links;
+			return array_merge( array(
+					'download' => '<a href="' . get_bloginfo( 'wpurl' ) . '/wp-admin/tools.php?page=mwpe_permalinks-export&noheader=true">Export Permalinks</a>'
+				), $links
+			);
+		}
+
+		public function admin_menu() {
+			add_submenu_page( null, "", "", "activate_plugins", "mwpe_permalinks-export", array( $this, "permalinks_list_export" ) );
+		}
+
+		public function permalinks_list_export() {
+			ob_clean();
+			header('Content-type:text/plain');
+			$posts = new WP_Query('post_type=any&posts_per_page=-1&suppress_filters=1&post_status=publish');
+			$posts = $posts->posts;
+			echo "Type\tTitle\tPermalink\n";
+			foreach($posts as $post) {
+				switch ($post->post_type) {
+					case 'revision':
+					case 'nav_menu_item':
+						break;
+					case 'page':
+						$permalink = get_page_link($post->ID);
+						break;
+					case 'post':
+						$permalink = get_permalink($post->ID);
+						break;
+					case 'attachment':
+						//$permalink = get_attachment_link($post->ID);
+						break;
+					default:
+						//$permalink = get_post_permalink($post->ID);
+						break;
+				}
+				echo "{$post->post_type}\t{$post->post_title}\t{$permalink}\n";
+			}
+			exit;
+		}
+
+	}
+
+	$GLOBALS['Meow_Permalink_List'] = new Meow_Permalink_List;
+
+}
+
+?>
